7ed0d9521ea491d9ed91e8bc44acf5c5d1bd2c4f
Emily Ren <Seattle.Emily@gmail.com>
2025-10-04 17:45:12 -0700

    add K6script.js

---
+++ k6-demo/K6script.js
@@ -0,0 +1,15 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export let options = {
+  vus: 10,  // number of virtual users
+  duration: '30s',
+};
+
+export default function () {
+  let res = http.get('https://jsonplaceholder.typicode.com/posts');
+  check(res, {
+    'status is 200': (r) => r.status === 200,
+  });
+  sleep(1);
+}
